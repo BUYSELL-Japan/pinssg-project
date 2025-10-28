d8b72f619de9593f9bccf5c898464c6a453575d7
Hiroaki <buyselljapan0311@gmail.com>
2025-10-29 06:29:40 +0900

    Fix asset paths from relative to absolute

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -4,11 +4,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  base: '/',
   build: {
     format: 'file',
-    inlineStylesheets: 'always'
+    inlineStylesheets: 'always',
+    assetsPrefix: '/'
   },
   vite: {
+    base: '/',
     build: {
       minify: 'esbuild',
       cssCodeSplit: false
